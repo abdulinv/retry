86e82b5de58312ab1aecfedbdbcbd1f0dd539255
Althaf <abdulla@innoventes.co>
2024-12-15 10:19:14 +0530

    Remove init button from manage page

--- src/app/manage/[slug]/CardContainer.tsx
+++ src/app/manage/[slug]/CardContainer.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import React from "react";
-import { Grid2,Button } from "@mui/material";
+import { Grid2 } from "@mui/material";
 import DayCard from "../DayCard";
 import { useParams } from "next/navigation";
 import { daily, monthly, Tasks, weekly } from "@/models/checklist/daily/daily";
@@ -9,25 +9,23 @@ import { daily, monthly, Tasks, weekly } from "@/models/checklist/daily/daily";
 interface CardContainerProps {
   data: Tasks[];
 }
- const init = ()=>{
-    localStorage.setItem("daily",JSON.stringify(daily));
-    localStorage.setItem("weekly",JSON.stringify(weekly));
-    localStorage.setItem("monthly",JSON.stringify(monthly));
- }
+const init = () => {
+  localStorage.setItem("daily", JSON.stringify(daily));
+  localStorage.setItem("weekly", JSON.stringify(weekly));
+  localStorage.setItem("monthly", JSON.stringify(monthly));
+};
 
 function CardContainer({ data }: CardContainerProps) {
   const params = useParams();
   const slug = params.slug as string;
-  const tasks = data.filter(item=>item.category === slug);
+  const tasks = data.filter((item) => item.category === slug);
   let size;
   if (slug === "Daily") {
     size = 4;
-  }
-  else if (slug === "Weekly") size = 5;
+  } else if (slug === "Weekly") size = 5;
   else size = 6;
   return (
     <>
-      <Button onClick={init} variant="contained">Init</Button>
       <Grid2 container gap={2} rowGap={6} justifyContent={"space-evenly"}>
         {tasks.map((day: Tasks, i: number) => {
           return (
