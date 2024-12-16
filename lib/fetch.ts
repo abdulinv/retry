24c2a8387e6741ab12d5842ca097205e33bf111e
Althaf <abdulla@innoventes.co>
2024-12-16 21:19:59 +0530

    Add option for new stack

--- lib/fetch.ts
+++ lib/fetch.ts
@@ -1,10 +1,16 @@
-"use server"
+"use server";
 
-import { collection, getDocs, updateDoc, doc,addDoc} from "firebase/firestore";
+import {
+  collection,
+  getDocs,
+  updateDoc,
+  doc,
+  addDoc,
+} from "firebase/firestore";
 import { db } from "@/firebase/config";
 import { Tasks } from "@/models/checklist/daily/daily";
 import { revalidatePath } from "next/cache";
-import {  RoadMaps } from "@/app/roadmap/types";
+import { RoadMaps } from "@/app/roadmap/types";
 
 export async function getTasks() {
   const data: Tasks[] = [];
@@ -26,30 +32,45 @@ export async function getTasks() {
 export async function updateTask(
   collection: string,
   taskId: string,
-  updatedData:Tasks | RoadMaps
+  updatedData: Tasks | RoadMaps
 ) {
   const taskRef = doc(db, collection, taskId);
   await updateDoc(taskRef, updatedData);
   console.log("Task updated!");
-  revalidatePath("manage/Daily")
+  revalidatePath("manage/Daily");
 }
 
-export async function addDocument(collectionId:string,doc:RoadMaps) {
+export async function addDocument(collectionId: string, doc: RoadMaps) {
   try {
     const docRef = await addDoc(collection(db, collectionId), doc);
     console.log("Document written with ID: ", docRef.id);
-    revalidatePath("manage/Daily")
+    revalidatePath("manage/Daily");
   } catch (e) {
     console.error("Error adding document: ", e);
   }
 }
 
 export async function getRoadMaps() {
-  const data: {id:string,doc:RoadMaps}[] = [];
-  const querySnapshot = await getDocs(collection(db, "rm-TypeScript"));
-  querySnapshot.forEach((doc) => {
-    console.log(`${doc.id} => `, doc.data());
-    data.push({id:doc.id,doc:doc.data() as RoadMaps});
-  });
+  const rmCollection = ["rm-TypeScript","rm-React"];
+  const data: { id: string; doc: RoadMaps }[] = [];
+
+  for (const collectionName of rmCollection) {
+    const querySnapshot = await getDocs(collection(db, collectionName));
+    querySnapshot.forEach((doc) => {
+      console.log(`${doc.id} => `, doc.data());
+      data.push({ id: doc.id, doc: doc.data() as RoadMaps });
+    });
+  }
+
   return data;
 }
+
+// export async function createNewStack(collectionId:string){
+//   try {
+//     const docRef = await addDoc(collection(db, collectionId), doc);
+//     console.log("Document written with ID: ", docRef.id);
+//     revalidatePath("manage/Daily")
+//   } catch (e) {
+//     console.error("Error adding document: ", e);
+//   }
+// }
--- src/app/roadmap/StackList.tsx
+++ src/app/roadmap/StackList.tsx
@@ -1,21 +1,70 @@
-"use client"
+"use client";
 
-import { Grid2, List, ListItem, ListItemButton } from "@mui/material";
+import {
+  Button,
+  Grid2,
+  List,
+  ListItem,
+  ListItemButton,
+  TextField,
+  Typography,
+} from "@mui/material";
 
 import React from "react";
-import { RoadMapDoc} from "./types";
+import { RoadMapDoc } from "./types";
+import { useState } from "react";
+import { addDocument } from "../../../lib/fetch";
 
 interface RoadMapProps {
   RoadMaps: RoadMapDoc[];
-  setSelectedStack:(stack:string)=>void
+  setSelectedStack: (stack: string) => void;
 }
 
-function StackList({ RoadMaps,setSelectedStack }: RoadMapProps) {
-  const stacklist = [...new Set(RoadMaps.map(item=>item.doc.stack))] 
+function StackList({ RoadMaps, setSelectedStack }: RoadMapProps) {
+  const [showInput, setShowInput] = useState("");
+  const [value, setValue] = useState("");
+  console.log("road maps data",RoadMaps);
+  const stacklist = [...new Set(RoadMaps.map((item) => item.doc.stack))];
   return (
     <Grid2 sx={{ border: "1px solid grey", borderRadius: "12px" }} size={2}>
+      <Button onClick={() => setShowInput("add new")}>
+        {showInput !== "add new" && (
+          <Typography variant="body2">add new</Typography>
+        )}
+        {showInput === "add new" && (
+          <TextField
+            value={value}
+            autoFocus
+            size="small"
+            fullWidth
+            onChange={(e) => setValue(e.target.value)}
+            onBlur={() => {
+              const newDoc = {
+                title: "Enter title here",
+                topics: ["add here"],
+                stack: value,
+              };
+              addDocument(`rm-${value}`, newDoc);
+              setShowInput("");
+            }}
+            sx={{
+              "& .MuiOutlinedInput-root": {
+                "& fieldset": {
+                  border: "none", // Remove the default border
+                },
+                "&:hover fieldset": {
+                  border: "none", // Prevent border on hover
+                },
+                "&.Mui-focused fieldset": {
+                  border: "none", // Prevent border on focus
+                },
+              },
+            }}
+          />
+        )}
+      </Button>
       <List>
-        {stacklist.map((item:string) => (
+        {stacklist.map((item: string) => (
           <ListItem key={item}>
             <ListItemButton onClick={() => setSelectedStack(item)}>
               {item}
